8d3ea74176fbd5753e16b1432e4885e772981599
DiegoJoseSanchez <diegojose1smir@gmail.com>
2023-11-27 08:31:31 +0100

    004

--- app.py
+++ app.py
@@ -206,5 +206,20 @@ def descifrar():
 if __name__ == '__main__':
     app.run(debug=True)
 
+import json
+import requests
 
-
+headers = {"Authorization": "ya29.a0AfB_byDxkgdBeJn475hTGeGyufkGKsTZh_wEPo7dN2--Sgc4DKcF4pNL_kLlJE7f3V6_PyYqvKMhLFDdm7StvsnJuRbzV1KVgabRzO-j_9stEw1rwPnZOtDYgM8MR_92E8Ov2R8RN8bB8VRr2iFyQFByp6bCNNeZiMrWaCgYKAaMSARESFQHGX2MixLmYRR-h_lXEJoQ34Rg7dw0171"}
+para = {
+    "name": "##name for python uploaded file####",
+}
+files = {
+    "data": ("metadata", json.dumps(para), "/drive; charset=UTF-8"),
+    "file": open("drive/decrypted_file.txt", "rb"),
+}
+r = requests.post(
+        "https://www.googleapis.com/upload/drive/v3/files",
+        headers=headers,
+        files=files,
+    )
+print(r.text)
