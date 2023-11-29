32f9195f82f536aa42351d865b4d69dccc84b8d1
DiegoJoseSanchez <diegojose1smir@gmail.com>
2023-11-29 11:06:31 +0100

    funcional

--- app.py
+++ app.py
@@ -1,13 +1,194 @@
-from datetime import datetime
+# from flask import Flask, render_template, request, send_file
+# from cryptography.fernet import Fernet
+# import os
+# from werkzeug.utils import secure_filename
+
+# app = Flask(__name__)
+
+# # Función para cargar la clave
+# def cargar_clave():
+#     if os.path.isfile('key.key'):
+#         with open('key.key', 'rb') as key_file:
+#             return key_file.read()
+#     return None
+
+# # Ruta para descargar la clave
+# @app.route('/descargar_clave', methods=['GET'])
+# def descargar_clave():
+#     key = cargar_clave()
+#     if key:
+#         with open('key.key', 'wb') as key_file:
+#             key_file.write(key)
+#         return send_file('key.key', as_attachment=True)
+#     return "No se ha generado ninguna clave aún."
+
+# # Ruta principal
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# # Ruta para cifrar archivos
+# @app.route('/cifrar', methods=['POST'])
+# def cifrar():
+#     if 'file' not in request.files or 'key' not in request.files:
+#         return "No se ha seleccionado algún archivo o clave"
+
+#     file = request.files['file']
+#     key_file = request.files['key']
+
+#     if file.filename == '' or key_file.filename == '':
+#         return "No se ha seleccionado algún archivo o clave"
+
+#     file_contents = file.read()
+#     key = key_file.read()
+
+#     cipher_suite = Fernet(key)
+#     encrypted_data = cipher_suite.encrypt(file_contents)
+
+#     # Obtén la opción de guardado del formulario
+#     save_option = request.form.get('save_option')
+
+#     if save_option == 'local':
+#         # Guardar localmente
+#         with open('encrypted_file.txt', 'wb') as encrypted_file:
+#             encrypted_file.write(encrypted_data)
+#         return send_file('encrypted_file.txt', as_attachment=True)
+#     elif save_option == 'destino':
+#         # Guardar en la carpeta de destino
+#         carpeta_destino = '\\\\DESKTOP-2HO19U6\\Colombia is Back'
+#         if not os.path.exists(carpeta_destino):
+#             os.makedirs(carpeta_destino)
+
+#         # Utilizar el nombre del archivo cifrado para construir la ruta de destino
+#         nombre_archivo_cifrado = secure_filename(file.filename)
+#         ruta_destino = os.path.join(carpeta_destino, nombre_archivo_cifrado + '_cifrado.txt')
+
+#         with open(ruta_destino, 'wb') as encrypted_file:
+#             encrypted_file.write(encrypted_data)
+#         return f'Archivo cifrado guardado con éxito en {ruta_destino}'
+
+#     return "Opción de guardado no válida"
+
+# # Ruta para descifrar archivos
+# @app.route('/descifrar', methods=['POST'])
+# def descifrar():
+#     if 'file' not in request.files or 'key' not in request.files:
+#         return "No se ha seleccionado algún archivo o clave"
+
+#     file = request.files['file']
+#     key_file = request.files['key']
+
+#     if file.filename == '' or key_file.filename == '':
+#         return "No se ha seleccionado algún archivo o clave"
+
+#     encrypted_data = file.read()
+#     key = key_file.read()
+
+#     cipher_suite = Fernet(key)
+
+#     # Obtén la opción de descifrado del formulario
+#     decrypt_option = request.form.get('decrypt_option')
+
+#     if decrypt_option == 'local':
+#         # Descifrar localmente
+#         try:
+#             decrypted_data = cipher_suite.decrypt(encrypted_data)
+#             with open('decrypted_file.txt', 'wb') as decrypted_file:
+#                 decrypted_file.write(decrypted_data)
+#             return send_file('decrypted_file.txt', as_attachment=True)
+#         except Exception as e:
+#             return f"Error al descifrar el archivo local: {str(e)}"
+#     elif decrypt_option == 'destino':
+#         # Descifrar en la carpeta de destino
+#         carpeta_destino = '\\\\DESKTOP-2HO19U6\\Colombia is Back'
+#         if not os.path.exists(carpeta_destino):
+#             os.makedirs(carpeta_destino)
+
+#         # Utilizar el nombre del archivo cifrado para construir la ruta de destino
+#         nombre_archivo_cifrado = secure_filename(file.filename)
+#         ruta_destino = os.path.join(carpeta_destino, nombre_archivo_cifrado.replace('_cifrado.txt', '_descifrado.txt'))
+
+#         try:
+#             decrypted_data = cipher_suite.decrypt(encrypted_data)
+#             with open(ruta_destino, 'wb') as decrypted_file:
+#                 decrypted_file.write(decrypted_data)
+#             return f'Archivo descifrado guardado con éxito en {ruta_destino}'
+#         except Exception as e:
+#             return f"Error al descifrar el archivo en la carpeta de destino: {str(e)}"
+
+#     return "Opción de descifrado no válida"
+
+# # Ruta para subir archivos
+# @app.route('/subir_archivo', methods=['POST'])
+# def subir_archivo():
+#     if 'archivo' not in request.files:
+#         return 'No se encontró el archivo en la solicitud'
+
+#     archivo = request.files['archivo']
+
+#     if archivo.filename == '':
+#         return 'No se seleccionó ningún archivo'
+
+#     # Ruta de destino en la red
+#     carpeta_destino = '\\\\DESKTOP-2HO19U6\\Colombia is Back'
+
+#     # Asegúrate de que la carpeta de destino exista
+#     if not os.path.exists(carpeta_destino):
+#         os.makedirs(carpeta_destino)
+
+#     # Ruta completa del archivo en la carpeta de destino
+#     ruta_destino = os.path.join(carpeta_destino, secure_filename(archivo.filename))
+
+#     # Copiar el archivo a la carpeta de destino
+#     archivo.save(ruta_destino)
+
+#     return f'Archivo subido con éxito a {ruta_destino}'
+
+# # Ruta para descargar archivos desde la carpeta compartida
+# @app.route('/descargar_archivo', methods=['GET'])
+# def descargar_archivo():
+#     nombre_archivo = request.args.get('nombre_archivo')
+
+#     if not nombre_archivo:
+#         return 'Nombre de archivo no proporcionado'
+
+#     # Construir la ruta completa del archivo en la carpeta compartida
+#     ruta_archivo = os.path.join('\\\\DESKTOP-2HO19U6\\Colombia is Back', nombre_archivo)
+
+#     # Verificar si el archivo existe
+#     if not os.path.exists(ruta_archivo):
+#         return 'El archivo no existe'
+
+#     # Descargar el archivo al cliente
+#     return send_file(ruta_archivo, as_attachment=True)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask, render_template, request, send_file
 from cryptography.fernet import Fernet
-##############################################
-from flask import Flask, render_template, request, send_file
-from cryptography.fernet import Fernet
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 
+# Función para cargar la clave
+def cargar_clave():
+    if os.path.isfile('key.key'):
+        with open('key.key', 'rb') as key_file:
+            return key_file.read()
+    return None
+
+# Función para descifrar los datos
+def descifrar_datos(encrypted_data, key):
+    cipher_suite = Fernet(key)
+    try:
+        decrypted_data = cipher_suite.decrypt(encrypted_data)
+        return decrypted_data
+    except Exception as e:
+        raise Exception(f"Error al descifrar los datos: {str(e)}")
+
+# Ruta para descargar la clave
 @app.route('/descargar_clave', methods=['GET'])
 def descargar_clave():
     key = cargar_clave()
@@ -17,45 +198,12 @@ def descargar_clave():
         return send_file('key.key', as_attachment=True)
     return "No se ha generado ninguna clave aún."
 
-def cargar_clave():
-    if os.path.isfile('key.key'):
-        with open('key.key', 'rb') as key_file:
-            return key_file.read()
-    return None
-
+# Ruta principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cifrar', methods=['POST'])
-def cifrar():
-    if 'file' not in request.files or 'key' not in request.files:
-        return "No se ha seleccionado algún archivo o clave"
-
-    file = request.files['file']
-    key_file = request.files['key']
-    if file.filename == '' or key_file.filename == '':
-        return "No se ha seleccionado algún archivo o clave"
-
-    file_contents = file.read()
-    key = key_file.read()
-
-    cipher_suite = Fernet(key)
-    encrypted_data = cipher_suite.encrypt(file_contents)
-
-    with open('encrypted_file.txt', 'wb') as encrypted_file:
-        encrypted_file.write(encrypted_data)
-
-    return send_file('encrypted_file.txt', as_attachment=True)
-##############################################
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
+# Ruta para cifrar archivos
 @app.route('/cifrar', methods=['POST'])
 def cifrar():
     if 'file' not in request.files or 'key' not in request.files:
@@ -87,14 +235,17 @@ def cifrar():
         if not os.path.exists(carpeta_destino):
             os.makedirs(carpeta_destino)
 
-        ruta_destino = os.path.join(carpeta_destino, file.filename + '_cifrado.txt')
+        # Utilizar el nombre del archivo cifrado para construir la ruta de destino
+        nombre_archivo_cifrado = secure_filename(file.filename)
+        ruta_destino = os.path.join(carpeta_destino, nombre_archivo_cifrado + '_cifrado.txt')
+
         with open(ruta_destino, 'wb') as encrypted_file:
             encrypted_file.write(encrypted_data)
         return f'Archivo cifrado guardado con éxito en {ruta_destino}'
 
     return "Opción de guardado no válida"
 
-# Resto del código original que proporcionaste
+# Ruta para descifrar archivos
 @app.route('/descifrar', methods=['POST'])
 def descifrar():
     if 'file' not in request.files or 'key' not in request.files:
@@ -102,36 +253,20 @@ def descifrar():
 
     file = request.files['file']
     key_file = request.files['key']
+
     if file.filename == '' or key_file.filename == '':
         return "No se ha seleccionado algún archivo o clave"
 
     encrypted_data = file.read()
     key = key_file.read()
 
-    cipher_suite = Fernet(key)
     try:
-        decrypted_data = cipher_suite.decrypt(encrypted_data)
+        decrypted_data = descifrar_datos(encrypted_data, key)
         with open('decrypted_file.txt', 'wb') as decrypted_file:
             decrypted_file.write(decrypted_data)
         return send_file('decrypted_file.txt', as_attachment=True)
     except Exception as e:
         return f"Error al descifrar el archivo: {str(e)}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-#Si lees esto has hecho un pull bien :D
-
-from datetime import datetime
-from flask import Flask, render_template, request, send_file
-from cryptography.fernet import Fernet
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
 
 # Ruta para subir archivos
 @app.route('/subir_archivo', methods=['POST'])
@@ -152,15 +287,14 @@ def subir_archivo():
         os.makedirs(carpeta_destino)
 
     # Ruta completa del archivo en la carpeta de destino
-    ruta_destino = os.path.join(carpeta_destino, archivo.filename)
+    ruta_destino = os.path.join(carpeta_destino, secure_filename(archivo.filename))
 
     # Copiar el archivo a la carpeta de destino
     archivo.save(ruta_destino)
 
     return f'Archivo subido con éxito a {ruta_destino}'
-#Prueba nueva
-# ...
 
+# Ruta para descargar archivos desde la carpeta compartida
 @app.route('/descargar_archivo', methods=['GET'])
 def descargar_archivo():
     nombre_archivo = request.args.get('nombre_archivo')
@@ -168,7 +302,7 @@ def descargar_archivo():
     if not nombre_archivo:
         return 'Nombre de archivo no proporcionado'
 
-    # Construir la ruta completa del archivo subido
+    # Construir la ruta completa del archivo en la carpeta compartida
     ruta_archivo = os.path.join('\\\\DESKTOP-2HO19U6\\Colombia is Back', nombre_archivo)
 
     # Verificar si el archivo existe
@@ -177,9 +311,6 @@ def descargar_archivo():
 
     # Descargar el archivo al cliente
     return send_file(ruta_archivo, as_attachment=True)
-
-
-#Fin de la prueba
 
 if __name__ == '__main__':
     app.run(debug=True)
